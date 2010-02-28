7ff3b0beca376fdf034c1392b590db4e79127e76
wendel.hades <wendel.hades@6ff7bca0-94c6-11de-adcb-5b537d459081>
2010-02-28 14:45:30 +0000

    NEW - task 1: Criar Parser XML seguindo JSAPI http://code.google.com/p/soundwave/issues/detail?id=1

---
+++ soundwave/src/inf/furb/xml/JSMLParser.java
@@ -0,0 +1,17 @@
+package inf.furb.xml;
+
+import org.dom4j.io.SAXReader;
+
+/**
+ * Parser XML que suporta JSML.
+ * 
+ * @author wendel
+ */
+public class JSMLParser {
+	
+	public JSMLParser(){
+		SAXReader sax = new SAXReader();
+		
+	}
+	
+}
