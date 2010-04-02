00b70b8f14cc0e4f00d8629c7e80bddff0978c26
wendel.hades <wendel.hades@6ff7bca0-94c6-11de-adcb-5b537d459081>
2010-04-02 20:43:39 +0000

    STARTED - task 4: Implementação do sintetizador http://code.google.com/p/soundwave/issues/detail?id=4

---
+++ trunk/soundwave-mbrola-test/src/inf/furb/synthesis/mbrola/comp/NumToWordTest.java
@@ -0,0 +1,45 @@
+package inf.furb.synthesis.mbrola.comp;
+
+import java.math.BigInteger;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NumToWordTest {
+
+	@Test
+	public void unidades() {
+		Assert.assertEquals("zero", NumToWord.convert(0));
+		Assert.assertEquals("seis", NumToWord.convert(6));
+	}
+
+	@Test
+	public void dezenas() {
+		Assert.assertEquals("dezesseis", NumToWord.convert(16));
+		Assert.assertEquals("cinquenta e um", NumToWord.convert(51));
+	}
+
+	@Test
+	public void centenas() {
+		Assert.assertEquals("cem", NumToWord.convert(100));
+		Assert.assertEquals("seiscentos e sessenta e seis", NumToWord.convert(666));
+	}
+
+	@Test
+	public void milhar() {
+		Assert.assertEquals("um mil", NumToWord.convert(1000));
+		Assert.assertEquals("um mil, duzentos e trinta e quatro", NumToWord.convert(1234));
+		Assert.assertEquals("trezentos mil e um", NumToWord.convert(300001));
+	}
+
+	@Test
+	public void milhoes() {
+		Assert.assertEquals("seis milhões", NumToWord.convert(6000000));
+	}
+
+	@Test
+	public void bilhoes() {
+		Assert.assertEquals("novecentos bilhões", NumToWord.convert(new BigInteger("900000000000")));
+	}
+
+}
