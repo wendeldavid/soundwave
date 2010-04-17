e45905b19bc9bb12c33d5bf2d1bbd29bca46054d
wendel.hades <wendel.hades@6ff7bca0-94c6-11de-adcb-5b537d459081>
2010-04-17 21:10:52 +0000

    STARTED - task 4: Implementação do sintetizador http://code.google.com/p/soundwave/issues/detail?id=4

--- trunk/soundwave-mbrola/src/inf/furb/synthesis/mbrola/converter/SayAsConverter.java
+++ trunk/soundwave-mbrola/src/inf/furb/synthesis/mbrola/converter/SayAsConverter.java
@@ -7,6 +7,7 @@ import inf.furb.synthesis.jsml.SayAs;
 import inf.furb.synthesis.mbrola.comp.ComponentGlobals;
 import inf.furb.synthesis.mbrola.comp.IComponent;
 import inf.furb.synthesis.mbrola.comp.NumToWord;
+import inf.furb.synthesis.mbrola.comp.Speller;
 import inf.furb.synthesis.mbrola.comp.Text;
 
 class SayAsConverter implements IConverter{
@@ -14,9 +15,15 @@ class SayAsConverter implements IConverter{
 	@Override
 	public void convert(ISynthElement element, StringBuilder output) {
 		IComponent text = null;
-		if (SayAs.CLASS_NUMBER.equals(element.getAttribute(SayAs.CLASS).getValue())) {
-			String numToWord = NumToWord.convert(new BigInteger(element.getText()));
+		final String elementText = element.getText();
+		final String elementClass = element.getAttribute(SayAs.CLASS).getValue();
+		if (SayAs.CLASS_NUMBER.equals(elementClass)) {
+			String numToWord = NumToWord.convert(new BigInteger(elementText));
 			text = new Text(numToWord);
+			
+		}else if(SayAs.CLASS_LITERAL.equals(elementClass)) {
+			text = new Speller(elementText);
+			
 		}else {
 			text = new Text(element.getText());
 		}
