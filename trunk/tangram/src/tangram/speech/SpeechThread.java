e0ec3421a274f5fefd4ed499e5c4ca260c1ae29d
wendel.hades <wendel.hades@6ff7bca0-94c6-11de-adcb-5b537d459081>
2010-05-25 03:27:48 +0000

    STARTED - task 9: Implementação do Tangram. http://code.google.com/p/soundwave/issues/detail?id=9

--- trunk/tangram/src/tangram/speech/SpeechThread.java
+++ trunk/tangram/src/tangram/speech/SpeechThread.java
@@ -1,10 +1,12 @@
 package tangram.speech;
 
+import inf.furb.synthesis.ISynthesizer;
+
 public final class SpeechThread extends Thread {
 
 	private boolean async = false;
 	
-	public SpeechThread(Runnable target, boolean async) {
+	public SpeechThread(ISynthesizer target, boolean async) {
 		super(target);
 		this.setAsync(async);
 	}
