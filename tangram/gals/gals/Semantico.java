f8db4b59e61a0b5cf87c291c9b51fdea75653f37
wendel.hades <wendel.hades@6ff7bca0-94c6-11de-adcb-5b537d459081>
2010-06-09 00:27:15 +0000

    STARTED - task 9: Implementação do Tangram. http://code.google.com/p/soundwave/issues/detail?id=9

--- tangram/gals/gals/Semantico.java
+++
@@ -1,9 +0,0 @@
-package gals;
-
-public class Semantico implements Constants
-{
-    public void executeAction(int action, Token token)	throws SemanticError
-    {
-        System.out.println("Ação #"+action+", Token: "+token);
-    }	
-}
