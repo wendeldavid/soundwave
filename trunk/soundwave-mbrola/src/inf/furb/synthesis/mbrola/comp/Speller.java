669779ce2b2e12e667de5773ab41d05617da02fe
wendel.hades <wendel.hades@6ff7bca0-94c6-11de-adcb-5b537d459081>
2010-07-06 02:24:06 +0000

    STARTED - task 9: Implementação do Tangram. http://code.google.com/p/soundwave/issues/detail?id=9

--- trunk/soundwave-mbrola/src/inf/furb/synthesis/mbrola/comp/Speller.java
+++ trunk/soundwave-mbrola/src/inf/furb/synthesis/mbrola/comp/Speller.java
@@ -37,6 +37,26 @@ public final class Speller implements IComponent{
 
 	public static String spell(char c) {
 		switch (Character.toLowerCase(c)) {
+		case '0':
+			return "zero";
+		case '1':
+			return "um";
+		case '2':
+			return "dois";
+		case '3':
+			return "três";
+		case '4':
+			return "quatro";
+		case '5':
+			return "cinco";
+		case '6':
+			return "seis";
+		case '7':
+			return "sete";
+		case '8':
+			return "oito";
+		case '9':
+			return "nove";
 		case 'a':
 			return "a";
 		case 'b':
